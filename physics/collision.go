8d1a04a2bdbf41c361a95ff9dd21e219ecd7f3a9
Ashley <ashleycheung2412@gmail.com>
2022-08-27 22:15:32 +1000

    Changed collision detection function to more generic methods

--- physics/collision.go
+++ physics/collision.go
@@ -33,13 +33,29 @@ func FindCollisions(w *World) []Collision {
 
 			// Pass to the correct shape collision detector
 			if body1ShapeType == CircleType && body2ShapeType == CircleType {
-				doesCollide = CircleCircleCollision(body1, body2)
+				b1Circle := body1.Shape.(Circle)
+				b2Circle := body2.Shape.(Circle)
+				doesCollide = CircleCircleCollision(
+					b1Circle.Radius,
+					body1.Position,
+					b2Circle.Radius,
+					body2.Position)
 			} else if body1ShapeType == RectangleType && body2ShapeType == RectangleType {
-				doesCollide = RectangleRectangleCollision(body1, body2)
+				b1Rect := body1.Shape.(Rectangle)
+				b2Rect := body2.Shape.(Rectangle)
+				doesCollide = RectangleRectangleCollision(
+					b1Rect.Size,
+					body1.Position,
+					b2Rect.Size,
+					body2.Position)
 			} else if body1ShapeType == CircleType && body2ShapeType == RectangleType {
-				doesCollide = CircleRectangleCollision(body1, body2)
+				circle := body1.Shape.(Circle)
+				rect := body2.Shape.(Rectangle)
+				doesCollide = CircleRectangleCollision(circle.Radius, body1.Position, rect.Size, body2.Position)
 			} else if body1ShapeType == RectangleType && body2ShapeType == CircleType {
-				doesCollide = CircleRectangleCollision(body2, body1)
+				rect := body1.Shape.(Rectangle)
+				circle := body2.Shape.(Circle)
+				doesCollide = CircleRectangleCollision(circle.Radius, body2.Position, rect.Size, body1.Position)
 			} else {
 				panic(fmt.Sprintf("collisions between %s and %s not supported", body1ShapeType, body2ShapeType))
 			}
@@ -61,56 +77,69 @@ func FindCollisions(w *World) []Collision {
 }
 
 // Returns whether there is a collision between
-// two circle bodies. Assumes that b1 and b2 have circle
-// body shapes
-func CircleCircleCollision(b1, b2 *Body) bool {
-	b1Circle := b1.Shape.(Circle)
-	b2Circle := b2.Shape.(Circle)
-
-	dist := b1.Position.DistanceTo(b2.Position)
-	radiiSum := b1Circle.Radius + b2Circle.Radius
+// two circles
+func CircleCircleCollision(
+	circle1Radius float64,
+	circle1Position Vector,
+	circle2Radius float64,
+	circle2Position Vector,
+) bool {
+	dist := circle1Position.DistanceTo(circle2Position)
+	radiiSum := circle1Radius + circle2Radius
 	return dist <= radiiSum
 }
 
+// Detects circle and rectangle collision
 // Based off the first solution here
 // https://stackoverflow.com/questions/401847/circle-rectangle-collision-detection-intersection
-func CircleRectangleCollision(circleBody, rectangleBody *Body) bool {
-	circle := circleBody.Shape.(Circle)
-	rect := rectangleBody.Shape.(Rectangle)
+func CircleRectangleCollision(
+	circleRadius float64,
+	circlePosition Vector,
+	rectSize Vector,
+	rectPosition Vector,
+) bool {
 	circleDist := Vector{
-		X: math.Abs(circleBody.Position.X - rectangleBody.Position.X),
-		Y: math.Abs(circleBody.Position.Y - rectangleBody.Position.Y),
+		X: math.Abs(circlePosition.X - rectPosition.X),
+		Y: math.Abs(circlePosition.Y - rectPosition.Y),
 	}
 
-	if circleDist.X > (rect.Size.X/2 + circle.Radius) {
+	if circleDist.X > (rectSize.X/2 + circleRadius) {
 		return false
 	}
-	if circleDist.Y > (rect.Size.Y/2 + circle.Radius) {
+	if circleDist.Y > (rectSize.Y/2 + circleRadius) {
 		return false
 	}
 
-	if circleDist.X <= (rect.Size.X / 2) {
+	if circleDist.X <= (rectSize.X / 2) {
 		return true
 	}
-	if circleDist.Y <= (rect.Size.Y / 2) {
+	if circleDist.Y <= (rectSize.Y / 2) {
 		return true
 	}
 
-	cornerDistSqred := circleDist.DistanceSquaredTo(rect.Size.Scale(0.5))
+	cornerDistSqred := circleDist.DistanceSquaredTo(rectSize.Scale(0.5))
 
-	return cornerDistSqred <= math.Pow(circle.Radius, 2)
+	return cornerDistSqred <= math.Pow(circleRadius, 2)
 }
 
+// Detects whether two rectangles collide
 // Logic taken from here
 // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-func RectangleRectangleCollision(b1, b2 *Body) bool {
-	b1Rect := b1.Shape.(Rectangle)
-	b1TopLeft := b1.Position.Subtract(b1Rect.Size.Scale(0.5))
-	b1BottomRight := b1.Position.Add(b1Rect.Size.Scale(0.5))
+func RectangleRectangleCollision(
+	// The size of rectangle 1
+	rect1Size Vector,
+	// The position of rectangle 1
+	rect1Position Vector,
+	// The size of the rectangle 2
+	rect2Size Vector,
+	// The position of the rectangle 2
+	rect2Position Vector,
+) bool {
+	b1TopLeft := rect1Position.Subtract(rect1Size.Scale(0.5))
+	b1BottomRight := rect1Position.Add(rect1Size.Scale(0.5))
 
-	b2Rect := b2.Shape.(Rectangle)
-	b2TopLeft := b2.Position.Subtract(b2Rect.Size.Scale(0.5))
-	b2BottomRight := b2.Position.Add(b2Rect.Size.Scale(0.5))
+	b2TopLeft := rect2Position.Subtract(rect2Size.Scale(0.5))
+	b2BottomRight := rect2Position.Add(rect2Size.Scale(0.5))
 
 	return b1TopLeft.X < b2BottomRight.X &&
 		b1BottomRight.X > b2TopLeft.X &&
